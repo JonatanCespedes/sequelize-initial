db2fc743a99ee3672ce5b9c45adc211dd911d37d
Jona <develop.jec@gmail.com>
2021-09-22 20:08:36 -0300

    Consultas básicas

--- sequelize/controllers/indexController.js
+++ sequelize/controllers/indexController.js
@@ -1,4 +1,5 @@
 let db = require('../database/models')
+const { Op } = require('sequelize')
 
 module.exports = {
     index: (req, res) => {
@@ -11,6 +12,32 @@ module.exports = {
         })
     },
     detail: (req, res) => {
-        
+       /*  db.Songs.findOne({
+            where: {
+                id: req.params.id
+            }
+        }) */
+        db.Songs.findByPk(req.params.id)
+        .then(song => {
+            res.render("detail", {
+                title: song.nombre,
+                song
+            })
+        })
+    },
+    search: (req, res) => {
+        db.Songs.findAll({
+            where: {
+                nombre: {
+                    [Op.like] : `%${req.query.search}%`
+                } 
+            }
+        })
+        .then(songs => {
+            res.render("results", {
+                title: req.query.search,
+                songs
+            })
+        })
     }
 }
--- sequelize/routes/index.js
+++ sequelize/routes/index.js
@@ -5,5 +5,6 @@ let controller = require('../controllers/indexController')
 /* GET home page. */
 router.get('/', controller.index);
 router.get('/cancion/:id', controller.detail)
+router.get('/search', controller.search)
 
 module.exports = router;
